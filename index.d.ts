df901db7bc25f1c61fe6dee68d8904b09d695f2c
Fil Zembowicz <fil@filosophy.org>
2019-05-14 12:09:14 -0400

    Add TypeScript definitions file
    This allows the module to be used with type annotations.

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+
+declare module 'fetch-retry' {
+  type RequestDelayFunction = ((
+    attempt: number,
+    error: Error | null,
+    response: Response | null
+  ) => number);
+
+  type RequestRetryOnFunction = ((
+    attempt: number,
+    error: Error | null,
+    response: Response | null
+  ) => number);
+
+  interface IRequestInitWithRetry extends RequestInit {
+    retries?: number;
+    retryDelay?: number | RequestDelayFunction;
+    retryOn?: number[] | RequestRetryOnFunction;
+  }
+
+  function fetch(url: RequestInit, options: IRequestInitWithRetry): Promise<Response>;
+  export = fetch;
+}

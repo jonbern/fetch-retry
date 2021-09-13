120fdee4668f0d4024f42a65635695215e8567cc
Jon K. Bernhardsen <jonkristian@outlook.com>
2021-09-13 19:32:03 +0100

    Adds husky pre-commit script Performs rollup build for UMD dist and runs tests

--- dist/fetch-retry.umd.js
+++ dist/fetch-retry.umd.js
@@ -66,7 +66,8 @@
       // eslint-disable-next-line no-undef
       return new Promise(function (resolve, reject) {
         var wrappedFetch = function (attempt) {
-          fetch(input, init)
+          var _input = input instanceof Request ? input.clone() : input;
+          fetch(_input, init)
             .then(function (response) {
               if (Array.isArray(retryOn) && retryOn.indexOf(response.status) === -1) {
                 resolve(response);

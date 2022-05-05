2d74b6beda96db969b3a7716daae66a398cafdf4
Kushal Mahajan <kushal.mahajan@toppr.com>
2022-05-05 18:02:08 +0530

    formatting fix

--- index.js
+++ index.js
@@ -1,167 +1,159 @@
 "use strict";
 
 module.exports = function (fetch, defaults) {
-	defaults = defaults || {};
-	if (typeof fetch !== "function") {
-		throw new ArgumentError("fetch must be a function");
-	}
+  defaults = defaults || {};
+  if (typeof fetch !== "function") {
+    throw new ArgumentError("fetch must be a function");
+  }
 
-	if (typeof defaults !== "object") {
-		throw new ArgumentError("defaults must be an object");
-	}
-  
-	if (
-		defaults.retries !== undefined &&
-		!isPositiveInteger(defaults.retries)
-	) {
-		throw new ArgumentError("retries must be a positive integer");
-	}
+  if (typeof defaults !== "object") {
+    throw new ArgumentError("defaults must be an object");
+  }
 
-	if (
-		defaults.retryDelay !== undefined &&
-		!isPositiveInteger(defaults.retryDelay) &&
-		typeof defaults.retryDelay !== "function"
-	) {
-		throw new ArgumentError(
-			"retryDelay must be a positive integer or a function returning a positive integer"
-		);
-	}
+  if (defaults.retries !== undefined && !isPositiveInteger(defaults.retries)) {
+    throw new ArgumentError("retries must be a positive integer");
+  }
 
-	if (
-		defaults.retryOn !== undefined &&
-		!Array.isArray(defaults.retryOn) &&
-		typeof defaults.retryOn !== "function"
-	) {
-		throw new ArgumentError(
-			"retryOn property expects an array or function"
-		);
-	}
+  if (
+    defaults.retryDelay !== undefined &&
+    !isPositiveInteger(defaults.retryDelay) &&
+    typeof defaults.retryDelay !== "function"
+  ) {
+    throw new ArgumentError(
+      "retryDelay must be a positive integer or a function returning a positive integer"
+    );
+  }
 
-	var baseDefaults = {
-		retries: 3,
-		retryDelay: 1000,
-		retryOn: [],
-	};
+  if (
+    defaults.retryOn !== undefined &&
+    !Array.isArray(defaults.retryOn) &&
+    typeof defaults.retryOn !== "function"
+  ) {
+    throw new ArgumentError("retryOn property expects an array or function");
+  }
 
-	defaults = Object.assign(baseDefaults, defaults);
+  var baseDefaults = {
+    retries: 3,
+    retryDelay: 1000,
+    retryOn: [],
+  };
 
-	return function fetchRetry(input, init) {
-		var retries = defaults.retries;
-		var retryDelay = defaults.retryDelay;
-		var retryOn = defaults.retryOn;
+  defaults = Object.assign(baseDefaults, defaults);
 
-		if (init && init.retries !== undefined) {
-			if (isPositiveInteger(init.retries)) {
-				retries = init.retries;
-			} else {
-				throw new ArgumentError("retries must be a positive integer");
-			}
-		}
+  return function fetchRetry(input, init) {
+    var retries = defaults.retries;
+    var retryDelay = defaults.retryDelay;
+    var retryOn = defaults.retryOn;
 
-		if (init && init.retryDelay !== undefined) {
-			if (
-				isPositiveInteger(init.retryDelay) ||
-				typeof init.retryDelay === "function"
-			) {
-				retryDelay = init.retryDelay;
-			} else {
-				throw new ArgumentError(
-					"retryDelay must be a positive integer or a function returning a positive integer"
-				);
-			}
-		}
+    if (init && init.retries !== undefined) {
+      if (isPositiveInteger(init.retries)) {
+        retries = init.retries;
+      } else {
+        throw new ArgumentError("retries must be a positive integer");
+      }
+    }
 
-		if (init && init.retryOn) {
-			if (
-				Array.isArray(init.retryOn) ||
-				typeof init.retryOn === "function"
-			) {
-				retryOn = init.retryOn;
-			} else {
-				throw new ArgumentError(
-					"retryOn property expects an array or function"
-				);
-			}
-		}
+    if (init && init.retryDelay !== undefined) {
+      if (
+        isPositiveInteger(init.retryDelay) ||
+        typeof init.retryDelay === "function"
+      ) {
+        retryDelay = init.retryDelay;
+      } else {
+        throw new ArgumentError(
+          "retryDelay must be a positive integer or a function returning a positive integer"
+        );
+      }
+    }
 
-		// eslint-disable-next-line no-undef
-		return new Promise(function (resolve, reject) {
-			var wrappedFetch = async function (attempt) {
-				var _input =
-					typeof Request !== "undefined" && input instanceof Request
-						? input.clone()
-						: input;
-				try {
-					const response = await fetch(_input, init);
-					if (
-						Array.isArray(retryOn) &&
-						retryOn.indexOf(response.status) === -1
-					) {
-						resolve(response);
-					} else if (typeof retryOn === "function") {
-						try {
-							const retryOnResponse = await Promise.resolve(
-								retryOn(attempt, null, response)
-							);
+    if (init && init.retryOn) {
+      if (Array.isArray(init.retryOn) || typeof init.retryOn === "function") {
+        retryOn = init.retryOn;
+      } else {
+        throw new ArgumentError(
+          "retryOn property expects an array or function"
+        );
+      }
+    }
 
-							if (retryOnResponse) {
-								retry(attempt, null, response);
-							} else {
-								resolve(response);
-							}
-						} catch (error) {
-							reject(error);
-						}
-					} else {
-						if (attempt < retries) {
-							retry(attempt, null, response);
-						} else {
-							resolve(response);
-						}
-					}
-				} catch (error) {
-					if (typeof retryOn === "function") {
-						try {
-							const retryOnResponse = await Promise.resolve(
-								retryOn(attempt, error, null)
-							);
+    // eslint-disable-next-line no-undef
+    return new Promise(function (resolve, reject) {
+      var wrappedFetch = async function (attempt) {
+        var _input =
+          typeof Request !== "undefined" && input instanceof Request
+            ? input.clone()
+            : input;
+        try {
+          const response = await fetch(_input, init);
+          if (
+            Array.isArray(retryOn) &&
+            retryOn.indexOf(response.status) === -1
+          ) {
+            resolve(response);
+          } else if (typeof retryOn === "function") {
+            try {
+              const retryOnResponse = await Promise.resolve(
+                retryOn(attempt, null, response)
+              );
 
-							if (retryOnResponse) {
-								retry(attempt, error, null);
-							} else {
-								reject(error);
-							}
-						} catch (error) {
-							reject(error);
-						}
-					} else if (attempt < retries) {
-						retry(attempt, error, null);
-					} else {
-						reject(error);
-					}
-				}
-			};
+              if (retryOnResponse) {
+                retry(attempt, null, response);
+              } else {
+                resolve(response);
+              }
+            } catch (error) {
+              reject(error);
+            }
+          } else {
+            if (attempt < retries) {
+              retry(attempt, null, response);
+            } else {
+              resolve(response);
+            }
+          }
+        } catch (error) {
+          if (typeof retryOn === "function") {
+            try {
+              const retryOnResponse = await Promise.resolve(
+                retryOn(attempt, error, null)
+              );
 
-			function retry(attempt, error, response) {
-				var delay =
-					typeof retryDelay === "function"
-						? retryDelay(attempt, error, response)
-						: retryDelay;
-				setTimeout(function () {
-					wrappedFetch(++attempt);
-				}, delay);
-			}
+              if (retryOnResponse) {
+                retry(attempt, error, null);
+              } else {
+                reject(error);
+              }
+            } catch (error) {
+              reject(error);
+            }
+          } else if (attempt < retries) {
+            retry(attempt, error, null);
+          } else {
+            reject(error);
+          }
+        }
+      };
 
-			wrappedFetch(0);
-		});
-	};
+      function retry(attempt, error, response) {
+        var delay =
+          typeof retryDelay === "function"
+            ? retryDelay(attempt, error, response)
+            : retryDelay;
+        setTimeout(function () {
+          wrappedFetch(++attempt);
+        }, delay);
+      }
+
+      wrappedFetch(0);
+    });
+  };
 };
 
 function isPositiveInteger(value) {
-	return Number.isInteger(value) && value >= 0;
+  return Number.isInteger(value) && value >= 0;
 }
 
 function ArgumentError(message) {
-	this.name = "ArgumentError";
-	this.message = message;
+  this.name = "ArgumentError";
+  this.message = message;
 }
